364e1ac9cef837bcb4880b9644c0222d5c80f279
scotthovestadt <gamingforever@gmail.com>
2013-01-14 11:09:38 -0800

    Update GigyaExample/GigyaExampleViewController.m

--- GigyaExample/GigyaExampleViewController.m
+++ GigyaExample/GigyaExampleViewController.m
@@ -56,19 +56,19 @@ static NSString *apiKey = @"2_Y82PzwJ_chSFImHXaIDJClnLyJzmk-VFOavSsaNTzl6m901s_N
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Alert" message:@"You are already logged in." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
         [alertView show];
     } else {
-        //Load the Gigya login UI component, passing this View Controller as a delegate.
+        // Load the Gigya login UI component, passing this View Controller as a delegate.
         [gsAPI showLoginUI:nil ViewController:self delegate:self context:nil];
     }
 }
 
 - (IBAction)logoutPushed:(id)sender
 {
-    //Check if the user is already logged out
+    // Check if the user is already logged out
     if (![[gsAPI getSession] IsValid]){
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Alert" message:@"You are already logged out." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
         [alertView show];
     } else {
-        //Log the user out
+        // Log the user out
         [gsAPI logout];
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Alert" message:@"You are now logged out." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
         [alertView show];
@@ -81,30 +81,33 @@ static NSString *apiKey = @"2_Y82PzwJ_chSFImHXaIDJClnLyJzmk-VFOavSsaNTzl6m901s_N
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Alert" message:@"You must be logged in to share." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
         [alertView show];
     } else {
-        //Define a userAction to be published
+        // Define a userAction to be published
         GSObject *userAction = [GSObject new];
         [userAction putStringValue:@"This is my title" forKey:@"title"];
         [userAction putStringValue:@"http://www.gigya.com" forKey:@"linkBack"];
         [userAction putStringValue:@"This is my description." forKey:@"description"];
-        //Take the message from the textView
+        
+        // Take the message from the textView
         [userAction putStringValue:[textView text] forKey:@"userMessage"];
     
-        //Add an image to the userAction
+        // Add an image to the userAction
         GSObject *image = [GSObject new];
         [image putStringValue:@"http://www.gigya.com/wp-content/themes/gigyatm/images/gigya-logo.gif" forKey:@"src"];
         [image putStringValue:@"http://www.gigya.com" forKey:@"href"];
         [image putStringValue:@"image" forKey:@"type"];
     
-        //Create a media items array and add the image object to it
+        // Create a media items array and add the image object to it
         NSArray *mediaItems = [NSArray arrayWithObject:image];
-        //Add the media items to the user action object
+        
+        // Add the media items to the user action object
         [userAction putNSArrayValue:mediaItems forKey:@"mediaItems"];
     
         GSObject *pParams = [GSObject new];
-        //Add the user action object to the parameters for the API call
+        
+        // Add the user action object to the parameters for the API call
         [pParams putGSObjectValue:userAction forKey:@"userAction"];
     
-        //Send the request, using this View Controller as the delegate
+        // Send the request, using this View Controller as the delegate
         [gsAPI sendRequest:@"socialize.publishUserAction" params:pParams delegate:self context:nil];
     }
 }
@@ -114,7 +117,7 @@ static NSString *apiKey = @"2_Y82PzwJ_chSFImHXaIDJClnLyJzmk-VFOavSsaNTzl6m901s_N
     
 }
 
-//Fired when the login operation completes
+// Fired when the login operation completes
 - (void)  gsLoginUIDidLogin:(NSString*)provider user:(GSObject*)user context:(id)context;
 {
     NSString *msg = @"Welcome ";
@@ -126,19 +129,19 @@ static NSString *apiKey = @"2_Y82PzwJ_chSFImHXaIDJClnLyJzmk-VFOavSsaNTzl6m901s_N
     NSLog(@"gsLoginUIDidLogin: provider=%@ user=%@ context=%@",provider,[user stringValue],context);
 }
 
-//Fired when a Gigya API response is received
+// Fired when a Gigya API response is received
 - (void) gsDidReceiveResponse:(NSString*)method response:(GSResponse*)response context:(id)context
 {
     NSLog(@"Gigya response: %@", response.ResponseText);
     
     // Handle response here...
-    if (response.errorCode==0) {  // SUCCESS! response status = OK
+    if (response.errorCode == 0) {  // SUCCESS! response status = OK
         if (method == @"socialize.publishUserAction"){
             UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Alert" message:@"Action published." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
             [alertView show];
         }
     } else {
-		NSLog(@"Gigya error: %@", response.errorMessage);
+	NSLog(@"Gigya error: %@", response.errorMessage);
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Alert" message:@"API Call failed. Check the log for errors." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
         [alertView show];
     }
